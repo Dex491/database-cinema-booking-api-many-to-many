132d38a0f06ca5a2d9fa46e4c79bec89a22c0e76
Dave <Daverevell491@gmail.com>
2023-01-20 18:36:54 +0000

    Seats populated and connecting with tickets and screen

---
+++ prisma/migrations/20230120182434_new_model/migration.sql
@@ -0,0 +1,13 @@
+/*
+  Warnings:
+
+  - Added the required column `screenId` to the `Seat` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `seatNumber` to the `Seat` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "Seat" ADD COLUMN     "screenId" INTEGER NOT NULL,
+ADD COLUMN     "seatNumber" INTEGER NOT NULL;
+
+-- AddForeignKey
+ALTER TABLE "Seat" ADD CONSTRAINT "Seat_screenId_fkey" FOREIGN KEY ("screenId") REFERENCES "Screen"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
